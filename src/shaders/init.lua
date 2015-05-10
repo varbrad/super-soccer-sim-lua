dbc2e54edf738b8949ccd7536f9a39b6359c0ff1
Brad Davies <bot-brad@gmx.com>
2015-05-10 17:14:19 +0100

    seascape removed, test_shader renamed 'inverted'
    default shader set to wavy (activated with "F12")
    ready for merge into master

--- src/shaders/init.lua
+++ src/shaders/init.lua
@@ -3,16 +3,13 @@ s.name = "Shaders Library"
 s.t = 0
 
 function s.init()
-	s.inverted = love.graphics.newShader("src/shaders/test_shader.frag")
+	s.inverted = love.graphics.newShader("src/shaders/inverted.frag")
 	s.wavy = love.graphics.newShader("src/shaders/wavy.frag")
-	s.seascape = love.graphics.newShader("src/shaders/seascape.frag")
-	s.seascape:send("iResolution", {g.width, g.height})
 end
 
 function s.update(dt)
 	s.t = s.t + dt
 	s.wavy:send("time",s.t)
-	s.seascape:send("iGlobalTime",s.t)
 end
 
 return s
